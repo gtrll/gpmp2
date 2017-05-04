d0d8d6912875a44281214cfb20fa15afd201b8d3
Mustafa Mukadam <mmukadam3@gatech.edu>
2017-05-04 14:17:36 -0400

    move sdf save/load to cpp

--- gpmp2/obstacle/SignedDistanceField.h
+++ gpmp2/obstacle/SignedDistanceField.h
@@ -187,43 +187,10 @@ public:
   }
 
   /// save to file
-  void saveSDF(const std::string filename) {
-    std::ofstream ofs(filename.c_str());
-    assert(ofs.good());
-    std::string fext = filename.substr(filename.find_last_of(".") + 1);
-    if (fext == "xml") {
-      boost::archive::xml_oarchive oa(ofs);
-      oa << BOOST_SERIALIZATION_NVP(this);
-    }
-    else if (fext == "bin") {
-      boost::archive::binary_oarchive oa(ofs);
-      oa << *this;
-    }
-    else {
-      boost::archive::text_oarchive oa(ofs);
-      oa << *this;
-    }
-  }
+  void saveSDF(const std::string filename);
 
-  // load from file
-  void loadSDF(const std::string filename) {
-    std::ifstream ifs(filename.c_str());
-    if (!ifs.good())
-      std::cout<<"File \'"<<filename<<"\' does not exist!"<<std::endl;
-    std::string fext = filename.substr(filename.find_last_of(".") + 1);
-    if (fext == "xml") {
-      boost::archive::xml_iarchive ia(ifs);
-      ia >> BOOST_SERIALIZATION_NVP(*this);
-    }
-    else if (fext == "bin") {
-      boost::archive::binary_iarchive ia(ifs);
-      ia >> *this;
-    }
-    else {
-      boost::archive::text_iarchive ia(ifs);
-      ia >> *this;
-    }
-  }
+  /// load from file
+  void loadSDF(const std::string filename);
 
 
 private:
