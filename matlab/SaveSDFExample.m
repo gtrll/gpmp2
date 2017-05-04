d8a77402f038d54801432ed00b23773588d64699
Mustafa Mukadam <mmukadam3@gatech.edu>
2017-05-04 14:18:20 -0400

    sdf load matlab example

---
+++ matlab/SaveSDFExample.m
@@ -0,0 +1,31 @@
+% Example to save sdf as gpmp2 datatype
+% @author Mustafa Mukadam
+% @date   May 4, 2017
+
+close all
+clear
+
+import gtsam.*
+import gpmp2.*
+
+
+%% dataset
+dataset = generate3Ddataset('WAMDeskDataset');
+origin = [dataset.origin_x, dataset.origin_y, dataset.origin_z];
+origin_point3 = Point3(origin');
+cell_size = dataset.cell_size;
+
+% init sdf
+disp('calculating signed distance field ...');
+field = signedDistanceField3D(dataset.map, dataset.cell_size);
+disp('calculating signed distance field done');
+
+sdf = SignedDistanceField(origin_point3, cell_size, size(field, 1), ...
+    size(field, 2), size(field, 3));
+for z = 1:size(field, 3)
+    sdf.initFieldData(z-1, field(:,:,z)');
+end
+
+%% save SDF
+disp('saving sdf to .bin file...');
+sdf.saveSDF('WAMDeskDataset.bin');
