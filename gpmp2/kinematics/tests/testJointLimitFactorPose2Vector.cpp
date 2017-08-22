706400e3aaa0038d6e9febb9f20b980f9dc3d55d
Jing Dong <jdong@gatech.edu>
2017-08-22 01:48:48 -0400

    joint limit factor on pose2vector, only vector space part

--- gpmp2/kinematics/tests/testJointLimitFactorPose2Vector.cpp
+++ gpmp2/kinematics/tests/testJointLimitFactorPose2Vector.cpp
@@ -1,18 +1,18 @@
 /**
-*  @file testJointLimitFactorVector.cpp
+*  @file testJointLimitFactorPose2Vector.cpp
 *  @author Jing Dong
 **/
 
 #include <CppUnitLite/TestHarness.h>
 
-#include <gtsam/base/numericalDerivative.h>
 #include <gtsam/inference/Symbol.h>
 #include <gtsam/nonlinear/NonlinearFactorGraph.h>
 #include <gtsam/nonlinear/GaussNewtonOptimizer.h>
 #include <gtsam/nonlinear/Values.h>
 #include <gtsam/slam/PriorFactor.h>
 
-#include <gpmp2/kinematics/JointLimitFactorVector.h>
+#include <gpmp2/kinematics/JointLimitFactorPose2Vector.h>
+#include <gpmp2/geometry/numericalDerivativeDynamic.h>
 
 #include <iostream>
 
@@ -22,7 +22,7 @@ using namespace gpmp2;
 
 
 /* ************************************************************************** */
-TEST(JointLimitFactorVector, error) {
+TEST(JointLimitFactorPose2Vector, error) {
 
   // settings
   noiseModel::Gaussian::shared_ptr cost_model = noiseModel::Isotropic::Sigma(2, 1.0);
@@ -30,56 +30,56 @@ TEST(JointLimitFactorVector, error) {
   // 2 link simple example
   Vector2 dlimit(-5.0, -10.0), ulimit(5, 10.0);
   Vector2 thresh(2.0, 2.0);
-  JointLimitFactorVector factor(0, cost_model, dlimit, ulimit, thresh);
-  Vector2 conf;
+  JointLimitFactorPose2Vector factor(0, cost_model, dlimit, ulimit, thresh);
+  Pose2Vector conf;
   Vector actual, expect;
   Matrix H_exp, H_act;
 
   // zero
-  conf = Vector2(0.0, 0.0);
+  conf = Pose2Vector(Pose2(), Vector2(0.0, 0.0));
   actual = factor.evaluateError(conf, H_act);
   expect = Vector2(0.0, 0.0);
-  H_exp = numericalDerivative11(boost::function<Vector2(const Vector2&)>(
-      boost::bind(&JointLimitFactorVector::evaluateError, factor, _1, boost::none)), conf, 1e-6);
+  H_exp = numericalDerivativeDynamic(boost::function<Vector(const Pose2Vector&)>(
+      boost::bind(&JointLimitFactorPose2Vector::evaluateError, factor, _1, boost::none)), conf, 1e-6);
   EXPECT(assert_equal(expect, actual, 1e-6));
   EXPECT(assert_equal(H_exp, H_act, 1e-6));
 
   // over down limit
-  conf = Vector2(-10.0, -10.0);
+  conf = Pose2Vector(Pose2(), Vector2(-10.0, -10.0));
   actual = factor.evaluateError(conf, H_act);
   expect = Vector2(7.0, 2.0);
-  H_exp = numericalDerivative11(boost::function<Vector2(const Vector2&)>(
-    boost::bind(&JointLimitFactorVector::evaluateError, factor, _1, boost::none)), conf, 1e-6);
+  H_exp = numericalDerivativeDynamic(boost::function<Vector(const Pose2Vector&)>(
+    boost::bind(&JointLimitFactorPose2Vector::evaluateError, factor, _1, boost::none)), conf, 1e-6);
   EXPECT(assert_equal(expect, actual, 1e-6));
   EXPECT(assert_equal(H_exp, H_act, 1e-6));
 
   // over up limit
-  conf = Vector2(10.0, 10.0);
+  conf = Pose2Vector(Pose2(), Vector2(10.0, 10.0));
   actual = factor.evaluateError(conf, H_act);
   expect = Vector2(7.0, 2.0);
-  H_exp = numericalDerivative11(boost::function<Vector2(const Vector2&)>(
-    boost::bind(&JointLimitFactorVector::evaluateError, factor, _1, boost::none)), conf, 1e-6);
+  H_exp = numericalDerivativeDynamic(boost::function<Vector(const Pose2Vector&)>(
+    boost::bind(&JointLimitFactorPose2Vector::evaluateError, factor, _1, boost::none)), conf, 1e-6);
   EXPECT(assert_equal(expect, actual, 1e-6));
   EXPECT(assert_equal(H_exp, H_act, 1e-6));
 }
 
 /* ************************************************************************** */
-TEST(JointLimitFactorVector, optimization_1) {
+TEST(JointLimitFactorPose2Vector, optimization_1) {
   // zero point
 
   // settings
   noiseModel::Gaussian::shared_ptr cost_model = noiseModel::Isotropic::Sigma(2, 0.001);
-  noiseModel::Gaussian::shared_ptr prior_model = noiseModel::Isotropic::Sigma(2, 1000);
+  noiseModel::Gaussian::shared_ptr prior_model = noiseModel::Isotropic::Sigma(5, 1000);
   Key qkey = Symbol('x', 0);
   Vector2 dlimit(-5.0, -10.0), ulimit(5, 10.0);
   Vector2 thresh(2.0, 2.0);
 
-  Vector conf;
-  conf = (Vector(2) << 0.0, 0.0).finished();
+  Pose2Vector conf;
+  conf = Pose2Vector(Pose2(), Vector2(0, 0));
 
   NonlinearFactorGraph graph;
-  graph.add(JointLimitFactorVector(qkey, cost_model, dlimit, ulimit, thresh));
-  graph.add(PriorFactor<Vector>(qkey, conf, prior_model));
+  graph.add(JointLimitFactorPose2Vector(qkey, cost_model, dlimit, ulimit, thresh));
+  graph.add(PriorFactor<Pose2Vector>(qkey, conf, prior_model));
   Values init_values;
   init_values.insert(qkey, conf);
 
@@ -91,26 +91,25 @@ TEST(JointLimitFactorVector, optimization_1) {
   Values results = optimizer.values();
 
   EXPECT_DOUBLES_EQUAL(0, graph.error(results), 1e-6);
-  EXPECT(assert_equal(conf, results.at<Vector>(qkey), 1e-6));
+  EXPECT(assert_equal(conf, results.at<Pose2Vector>(qkey), 1e-6));
 }
 
 /* ************************************************************************** */
-TEST(JointLimitFactorVector, optimization_2) {
+TEST(JointLimitFactorPose2Vector, optimization_2) {
   // over down limit
 
   // settings
   noiseModel::Gaussian::shared_ptr cost_model = noiseModel::Isotropic::Sigma(2, 0.001);
-  noiseModel::Gaussian::shared_ptr prior_model = noiseModel::Isotropic::Sigma(2, 1000);
+  noiseModel::Gaussian::shared_ptr prior_model = noiseModel::Isotropic::Sigma(5, 1000);
   Key qkey = Symbol('x', 0);
   Vector2 dlimit(-5.0, -10.0), ulimit(5, 10.0);
   Vector2 thresh(2.0, 2.0);
 
-  Vector conf;
-  conf = (Vector(2) << -10.0, -10.0).finished();
+  Pose2Vector conf(Pose2(1, -2, 3), Vector2(-10.0, -10.0));
 
   NonlinearFactorGraph graph;
-  graph.add(JointLimitFactorVector(qkey, cost_model, dlimit, ulimit, thresh));
-  graph.add(PriorFactor<Vector>(qkey, conf, prior_model));
+  graph.add(JointLimitFactorPose2Vector(qkey, cost_model, dlimit, ulimit, thresh));
+  graph.add(PriorFactor<Pose2Vector>(qkey, conf, prior_model));
   Values init_values;
   init_values.insert(qkey, conf);
 
@@ -122,26 +121,25 @@ TEST(JointLimitFactorVector, optimization_2) {
   Values results = optimizer.values();
 
   Vector conf_limit = (Vector(2) << -3.0, -8.0).finished();
-  EXPECT(assert_equal(conf_limit, results.at<Vector>(qkey), 1e-6));
+  EXPECT(assert_equal(conf_limit, results.at<Pose2Vector>(qkey).configuration(), 1e-6));
 }
 
 /* ************************************************************************** */
-TEST(JointLimitFactorVector, optimization_3) {
+TEST(JointLimitFactorPose2Vector, optimization_3) {
   // over up limit
 
   // settings
   noiseModel::Gaussian::shared_ptr cost_model = noiseModel::Isotropic::Sigma(2, 0.001);
-  noiseModel::Gaussian::shared_ptr prior_model = noiseModel::Isotropic::Sigma(2, 1000);
+  noiseModel::Gaussian::shared_ptr prior_model = noiseModel::Isotropic::Sigma(5, 1000);
   Key qkey = Symbol('x', 0);
   Vector2 dlimit(-5.0, -10.0), ulimit(5, 10.0);
   Vector2 thresh(2.0, 2.0);
 
-  Vector conf;
-  conf = (Vector(2) << 10.0, 10.0).finished();
+  Pose2Vector conf(Pose2(1, -2, 3), Vector2(10.0, 10.0));
 
   NonlinearFactorGraph graph;
-  graph.add(JointLimitFactorVector(qkey, cost_model, dlimit, ulimit, thresh));
-  graph.add(PriorFactor<Vector>(qkey, conf, prior_model));
+  graph.add(JointLimitFactorPose2Vector(qkey, cost_model, dlimit, ulimit, thresh));
+  graph.add(PriorFactor<Pose2Vector>(qkey, conf, prior_model));
   Values init_values;
   init_values.insert(qkey, conf);
 
@@ -153,7 +151,7 @@ TEST(JointLimitFactorVector, optimization_3) {
   Values results = optimizer.values();
 
   Vector conf_limit = (Vector(2) << 3.0, 8.0).finished();
-  EXPECT(assert_equal(conf_limit, results.at<Vector>(qkey), 1e-6));
+  EXPECT(assert_equal(conf_limit, results.at<Pose2Vector>(qkey).configuration(), 1e-6));
 }
 
 /* ************************************************************************** */
